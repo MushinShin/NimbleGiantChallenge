15284544af42cf5aac91e356570b3a5906ca124c
FacuFrancia <facundofrancia3@hotmail.com>
2021-10-19 18:26:09 -0300

    useless styles deleted sorted styles functionality and actions added

--- js/main.js
+++ js/main.js
@@ -1,20 +1,45 @@
 const mainQuestHeader = document.querySelector(".mainQuest-header");
 const sideQuestHeader = document.querySelector(".sideQuest-header");
+const navBtnQuests = document.getElementById("nav-btn-Quest");
+const navBtnSkills = document.getElementById("nav-btn-Skills");
+const arrowDropDown = document.getElementById("arrow-dropdown");
 const mainQuestList = document.querySelector(".list1");
 const sideQuestList = document.querySelector(".list2");
 
-let isRelative = true;
-const handleClick = () => {
-  let newValue = isRelative ? "relative" : "absolute";
-  mainQuestList.classList.toggle("newlist");
-  document.getElementById("list-main-quest").style.position = newValue;
-  isRelative = !isRelative;
+const skillsContainer = document.getElementById("skills-container");
+const questsContainer = document.getElementById("quests-container");
+
+const handleDropDownClick = (list) => {
+  const listElement = document.getElementById(list);
+  listElement.classList.toggle("newlist");
+
+  const arrowIcon = document.getElementById(`${list}_arrow`);
+  arrowIcon.classList.toggle("inverted");
+
+  const position = listElement.style.position;
+  if (!position || position === "absolute") {
+    listElement.style.position = "relative";
+    return;
+  }
+
+  listElement.style.position = "absolute";
 };
 
-const handleClick2 = () => {
-  sideQuestList.classList.toggle("newlist");
-  document.getElementById("list-side-quest").style.position = "relative";
+const handleTabChange = (tab) => {
+  if (tab === "quests") {
+    questsContainer.style.display = "block";
+    skillsContainer.style.display = "none";
+    navBtnQuests.children[0].style.backgroundColor = "#062438";
+    navBtnSkills.children[0].style.backgroundColor = "inherit";
+  } else if (tab === "skills") {
+    skillsContainer.style.display = "flex";
+    questsContainer.style.display = "none";
+    navBtnSkills.children[0].style.backgroundColor = "#062438";
+    navBtnQuests.children[0].style.backgroundColor = "inherit";
+  }
 };
 
-mainQuestHeader.onclick = handleClick;
-sideQuestHeader.onclick = handleClick2;
+navBtnQuests.onclick = () => handleTabChange("quests");
+navBtnSkills.onclick = () => handleTabChange("skills");
+mainQuestHeader.onclick = () => handleDropDownClick("list-main-quest");
+sideQuestHeader.onclick = () => handleDropDownClick("list-side-quest");
